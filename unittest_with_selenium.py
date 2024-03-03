66570379a003f389cfee1b433423e8c6f1b4b1ca
haziqa5122 <haziqa5122@gmail.com>
2024-03-03 23:46:59 +0500

    Modified grid URL

--- unittest_with_selenium.py
+++ unittest_with_selenium.py
@@ -30,7 +30,7 @@ def login_to_website(driver):
     '''
     Login to website for testing
     '''
-    # select the relevant fields to log into the website 
+    # select the relevant fields to log into the website
     username = driver.find_element("name", "user-name")
     password = driver.find_element("name", "password")
     submit_button = driver.find_element("name", "login-button")
@@ -45,7 +45,10 @@ def login_to_website(driver):
 
 class TestLoginPage(unittest.TestCase):
     def setUp(self):
-        
+
+        username = os.environ.get("USER")
+        access_key = os.environ.get("ACCESS_KEY")
+
         # load lambdaTest configurations
         lt_options = setup_lt_options()
 
@@ -56,9 +59,15 @@ class TestLoginPage(unittest.TestCase):
         options.set_capability('LT:Options', lt_options)
 
         # Start the browser
+        gridURL = "https://{}:{}@hub.lambdatest.com/wd/hub".format(
+            username, access_key)
         self.driver = webdriver.Remote(
-            command_executor="http://hub.lambdatest.com:80/wd/hub",
-            options=options)
+            command_executor=gridURL,
+            options=options
+        )
+        # self.driver = webdriver.Remote(
+        #     command_executor="http://hub.lambdatest.com:80/wd/hub",
+        #     options=options)
         self.driver.implicitly_wait(30)
         self.driver.maximize_window()
 
@@ -69,7 +78,7 @@ class TestLoginPage(unittest.TestCase):
         '''
         Test Login functionality of the page
         '''
-        # select the relevant fields to log into the website 
+        # select the relevant fields to log into the website
         username = self.driver.find_element("name", "user-name")
         password = self.driver.find_element("name", "password")
         submit_button = self.driver.find_element("name", "login-button")
@@ -81,11 +90,15 @@ class TestLoginPage(unittest.TestCase):
         # Click the submit button
         submit_button.click()
 
-        # Find products 
+        # Find products
         product_title = self.driver.find_element(By.CLASS_NAME, 'title')
 
         # check that the title is the same as displayed on the main page
-        self.assertEqual(product_title.text, "Products", "Main page title not found")
+        self.assertEqual(product_title.text, "Products",
+                         "Main page title not found")
+
+        # Indicate that the test has passed since the Assert is not raised
+        self.driver.execute_script("lambda-status=passed")
 
     def test_logout(self):
         '''
@@ -95,7 +108,8 @@ class TestLoginPage(unittest.TestCase):
         login_to_website(self.driver)
 
         # find hamburger menu button
-        hamburger_button = self.driver.find_element(By.ID, "react-burger-menu-btn")
+        hamburger_button = self.driver.find_element(
+            By.ID, "react-burger-menu-btn")
         # Click the submit button
         hamburger_button.click()
 
@@ -104,18 +118,26 @@ class TestLoginPage(unittest.TestCase):
         # Click the logout button
         logout_button.click()
 
-        # Find login button 
+        # Find login button
         login_btn = self.driver.find_element("name", "login-button")
         # assert if the Login button is displayed on the next page.
-        self.assertTrue(login_btn.is_displayed(), "Logout Unsuccessful, Login button not found on the final page")
+        self.assertTrue(login_btn.is_displayed(
+        ), "Logout Unsuccessful, Login button not found on the final page")
+
+        # Indicate that the test has passed since the Assert is not raised
+        self.driver.execute_script("lambda-status=passed")
 
     def tearDown(self):
         self.driver.quit()
 
+
 class TestPlatform(unittest.TestCase):
-    
+
     def setUp(self):
-        
+
+        username = os.environ.get("USER")
+        access_key = os.environ.get("ACCESS_KEY")
+
         # load lambdaTest configurations
         lt_options = setup_lt_options()
 
@@ -126,15 +148,21 @@ class TestPlatform(unittest.TestCase):
         options.set_capability('LT:Options', lt_options)
 
         # Start the browser
+        gridURL = "https://{}:{}@hub.lambdatest.com/wd/hub".format(
+            username, access_key)
         self.driver = webdriver.Remote(
-            command_executor="http://hub.lambdatest.com:80/wd/hub",
-            options=options)
+            command_executor=gridURL,
+            options=options
+        )
+        # self.driver = webdriver.Remote(
+        #     command_executor="http://hub.lambdatest.com:80/wd/hub",
+        #     options=options)
         self.driver.implicitly_wait(30)
         self.driver.maximize_window()
         # Go to the app home page
         self.driver.get("https://www.saucedemo.com/")
 
-        ## Login to the website by default
+        # Login to the website by default
         login_to_website(self.driver)
 
     def test_cart_badge_update(self):
@@ -144,31 +172,40 @@ class TestPlatform(unittest.TestCase):
 
         # Locate the add to cart button and click it
         add_to_cart_button = WebDriverWait(self.driver, 3).until(
-            EC.element_to_be_clickable(("name", 'add-to-cart-sauce-labs-backpack'))
+            EC.element_to_be_clickable(
+                ("name", 'add-to-cart-sauce-labs-backpack'))
         )
         add_to_cart_button.click()
 
         # Wait for the cart to update (you may need to adjust the time based on your website)
         WebDriverWait(self.driver, 3).until(
-            EC.text_to_be_present_in_element((By.CLASS_NAME, "shopping_cart_badge"), '1')
+            EC.text_to_be_present_in_element(
+                (By.CLASS_NAME, "shopping_cart_badge"), '1')
         )
-        badge_count = self.driver.find_element(By.CLASS_NAME, "shopping_cart_badge").text
+        badge_count = self.driver.find_element(
+            By.CLASS_NAME, "shopping_cart_badge").text
 
         self.assertEqual(badge_count, '1', "Cart count not updated to 1")
 
         # Locate the add to cart button and click it
         add_to_cart_button = WebDriverWait(self.driver, 3).until(
-            EC.element_to_be_clickable(("name", 'add-to-cart-sauce-labs-bike-light'))
+            EC.element_to_be_clickable(
+                ("name", 'add-to-cart-sauce-labs-bike-light'))
         )
         add_to_cart_button.click()
 
         # Wait for the cart to update (you may need to adjust the time based on your website)
         WebDriverWait(self.driver, 3).until(
-            EC.text_to_be_present_in_element((By.XPATH, '//*[@class="shopping_cart_badge"]'), '2')
+            EC.text_to_be_present_in_element(
+                (By.XPATH, '//*[@class="shopping_cart_badge"]'), '2')
         )
-        badge_count = self.driver.find_element(By.XPATH, '//*[@class="shopping_cart_badge"]').text
+        badge_count = self.driver.find_element(
+            By.XPATH, '//*[@class="shopping_cart_badge"]').text
 
         self.assertEqual(badge_count, '2', "Cart count not updated to 2")
+
+        # Indicate that the test has passed since the Assert is not raised
+        self.driver.execute_script("lambda-status=passed")
 
     def test_add_to_cart_functionality(self):
         '''
@@ -176,22 +213,30 @@ class TestPlatform(unittest.TestCase):
         '''
         # Locate the add to cart button and click it
         add_to_cart_button = WebDriverWait(self.driver, 3).until(
-            EC.element_to_be_clickable(("name", 'add-to-cart-sauce-labs-backpack'))
+            EC.element_to_be_clickable(
+                ("name", 'add-to-cart-sauce-labs-backpack'))
         )
         # Click add to cart button
         add_to_cart_button.click()
 
         # Wait for the cart to update (you may need to adjust the time based on your website)
         WebDriverWait(self.driver, 3).until(
-            EC.text_to_be_present_in_element((By.XPATH, '//*[@class="shopping_cart_badge"]'), '1')
+            EC.text_to_be_present_in_element(
+                (By.XPATH, '//*[@class="shopping_cart_badge"]'), '1')
         )
         # Click on cart button
-        cart_button = self.driver.find_element(By.CLASS_NAME, "shopping_cart_link")
+        cart_button = self.driver.find_element(
+            By.CLASS_NAME, "shopping_cart_link")
         cart_button.click()
 
-        item_name = self.driver.find_element(By.CLASS_NAME, "inventory_item_name").text
+        item_name = self.driver.find_element(
+            By.CLASS_NAME, "inventory_item_name").text
 
-        self.assertEqual(item_name, "Sauce Labs Backpack", "Added item not found in cart")
+        self.assertEqual(item_name, "Sauce Labs Backpack",
+                         "Added item not found in cart")
+
+        # Indicate that the test has passed since the Assert is not raised
+        self.driver.execute_script("lambda-status=passed")
 
     def test_continue_shopping(self):
         '''
@@ -203,28 +248,36 @@ class TestPlatform(unittest.TestCase):
 
         # Locate the add to cart button and click it
         add_to_cart_button = WebDriverWait(self.driver, 3).until(
-            EC.element_to_be_clickable(("name", 'add-to-cart-sauce-labs-backpack'))
+            EC.element_to_be_clickable(
+                ("name", 'add-to-cart-sauce-labs-backpack'))
         )
         # Click add to cart button
         add_to_cart_button.click()
 
         # Wait for the cart to update (you may need to adjust the time based on your website)
         WebDriverWait(self.driver, 3).until(
-            EC.text_to_be_present_in_element((By.XPATH, '//*[@class="shopping_cart_badge"]'), '1')
+            EC.text_to_be_present_in_element(
+                (By.XPATH, '//*[@class="shopping_cart_badge"]'), '1')
         )
         # Click on cart button
-        cart_button = self.driver.find_element(By.CLASS_NAME, "shopping_cart_link")
+        cart_button = self.driver.find_element(
+            By.CLASS_NAME, "shopping_cart_link")
         cart_button.click()
 
         # Click on continue shopping
-        continue_shopping = self.driver.find_element("name", 'continue-shopping')
+        continue_shopping = self.driver.find_element(
+            "name", 'continue-shopping')
         continue_shopping.click()
 
-        # Find products 
+        # Find products
         product_title = self.driver.find_element(By.CLASS_NAME, 'title')
 
         # check that the title is the same as displayed on the main page
-        self.assertEqual(product_title.text, "Products", "Main page title not found")
+        self.assertEqual(product_title.text, "Products",
+                         "Main page title not found")
+
+        # Indicate that the test has passed since the Assert is not raised
+        self.driver.execute_script("lambda-status=passed")
 
     def tearDown(self):
         self.driver.quit()
